0b4acc812a95a2772e739005b6939918e28bce15
Maia de Oliveira <fernandamaiadeoliveira@gmail.com>
2020-08-06 21:50:40 -0300

    server finalizado

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import ClassesController from "./controllers/ClassesControllers";
+import ConnectionsController from "./controllers/ConnectionsController";
 
 const routes = express.Router()
+const connectionsController = new ConnectionsController();
 
 const classesControllers = new ClassesController();
 
+routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
+
+routes.get('/connections', connectionsController.index)
+routes.post('/connections', connectionsController.create)
 
 export default routes;
